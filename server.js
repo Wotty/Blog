c612b56e035e1759a77193c00fecbbead3d7ef14
Will <Wotty@users.noreply.github.com>
2021-12-01 12:41:20 +0000

    Follow Tutorial

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const Article = require('./models/article')
-const articleRouter = require('./models/articles')
+const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
 const app = express()
-
 
 mongoose.connect('mongodb://localhost/blog', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
 })
 
 app.set('view engine', 'ejs')
@@ -23,4 +21,4 @@ app.get('/', async(req, res) => {
 
 app.use('/articles', articleRouter)
 
-app.listen(8080)
+app.listen(3000)
